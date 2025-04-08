059ad22d6acf4205eb4c4bbe880d9e80aa891479
Iris <148431646+iris915520@users.noreply.github.com>
2025-04-08 18:21:48 +0300

    update MainActivity

--- app/src/main/java/com/example/assignmentvi_intent/MainActivity.java
+++ app/src/main/java/com/example/assignmentvi_intent/MainActivity.java
@@ -1,24 +1,71 @@
 package com.example.assignmentvi_intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.assignmentvi_intent.model.Meeting;
+import com.example.assignmentvi_intent.model.MeetingManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity{
+    EditText et_title, et_place, et_participants, et_date, et_time;
+    Button btn_submit, btn_summary, btn_search, btn_update;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        et_title = findViewById(R.id.editTitle);
+        et_place = findViewById(R.id.editPlace);
+        et_participants = findViewById(R.id.editParticipants);
+        et_date = findViewById(R.id.editDate);
+        et_time = findViewById(R.id.editTime);
+
+        btn_submit = findViewById(R.id.btnSubmit);
+        btn_summary = findViewById(R.id.btnSummary);
+        btn_search = findViewById(R.id.btnSearch);
+        btn_update = findViewById(R.id.btnUpdate);
+
+        btn_submit.setOnClickListener(view -> {
+            String title = et_title.getText().toString();
+            String place = et_place.getText().toString();
+
+            String participantsText = et_participants.getText().toString();
+            List<String> participantsArray = Arrays.asList(participantsText.split(","));
+            List<String> participants = new ArrayList<>(participantsArray);
+
+            String date = et_date.getText().toString();
+            String time = et_time.getText().toString();
+
+            Meeting meeting = new Meeting(title, place, participants, date, time);
+            MeetingManager.addMeeting(meeting);
+
+            Intent intent = new Intent(MainActivity.this, SummaryActivity.class);
+            startActivity(intent);
         });
+
+        btn_summary.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, SummaryActivity.class);
+            startActivity(intent);
+        });
+
+        btn_search.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+            startActivity(intent);
+        });
+        btn_update.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, UpdateActivity.class);
+            startActivity(intent);
+        });
+
     }
+
 }
