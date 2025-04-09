34008c16426cab1a9d3855333821a41c361abf2c
Iris <148431646+iris915520@users.noreply.github.com>
2025-04-09 10:16:42 +0300

    update search method to return a list of meetings

--- app/src/main/java/com/example/assignmentvi_intent/model/MeetingManager.java
+++ app/src/main/java/com/example/assignmentvi_intent/model/MeetingManager.java
@@ -1,5 +1,7 @@
 package com.example.assignmentvi_intent.model;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class MeetingManager {
@@ -10,16 +12,19 @@ public class MeetingManager {
     public static List<Meeting> getMeetings(){
         return meetings;
     }
-    public static Meeting searchMeeting(String title, String place, String date, String time,String participants){
+    public static List<Meeting> searchMeeting(String title, String place, String date, String time, List<String> participants){
+        List<Meeting> results = new ArrayList<>();
         for (Meeting meeting : meetings){
             boolean result = (title == null || meeting.getTitle().equalsIgnoreCase(title) &&
                     place == null || meeting.getPlace().equalsIgnoreCase(place) &&
                     date == null || meeting.getDate().equalsIgnoreCase(date) &&
                     time == null || meeting.getTime().equalsIgnoreCase(time) &&
-                    participants == null || meeting.getParticipants().contains(participants));
-            if (result) return meeting;
+                    participants == null || new HashSet<>(meeting.getParticipants()).containsAll(participants));
+            if (result) {
+                results.add(meeting);
+            };
         }
-        return null;
+        return results;
     }
     public static void updateMeeting(Meeting oldMeeting, Meeting newMeeting){
         for (Meeting meeting : meetings){
