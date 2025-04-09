3afb786c9ff2034892631df1c9759357c102d697
Iris <148431646+iris915520@users.noreply.github.com>
2025-04-09 08:36:30 +0300

    add summary activity

--- app/src/main/java/com/example/assignmentvi_intent/SummaryActivity.java
+++ app/src/main/java/com/example/assignmentvi_intent/SummaryActivity.java
@@ -1,4 +1,34 @@
 package com.example.assignmentvi_intent;
 
-public class SummaryActivity {
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.assignmentvi_intent.model.Meeting;
+import com.example.assignmentvi_intent.model.MeetingManager;
+
+import java.util.List;
+
+public class SummaryActivity extends AppCompatActivity {
+    TextView summaryText;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+
+        summaryText = findViewById(R.id.summaryText);
+
+        StringBuilder summaryBuilder = new StringBuilder();
+        List<Meeting> meetings = MeetingManager.getMeetings();
+        for (Meeting meeting : meetings){
+            summaryBuilder.append("Title: ").append(meeting.getTitle()).append("\n")
+                    .append("Place: ").append(meeting.getPlace()).append("\n")
+                    .append("Participants: ").append(meeting.getParticipants()).append("\n")
+                    .append("Date: ").append(meeting.getDate()).append("\n")
+                    .append("Time: ").append(meeting.getTime()).append("\n\n");
+        }
+        summaryText.setText(summaryBuilder);
+    }
 }
