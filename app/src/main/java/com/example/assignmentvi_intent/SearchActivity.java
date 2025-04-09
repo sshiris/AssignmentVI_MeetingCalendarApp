15e02388204efc2166fb6e14977e59d93e6e59e8
Iris <148431646+iris915520@users.noreply.github.com>
2025-04-09 16:23:06 +0300

    search layout and activity

--- app/src/main/java/com/example/assignmentvi_intent/SearchActivity.java
+++ app/src/main/java/com/example/assignmentvi_intent/SearchActivity.java
@@ -1,4 +1,70 @@
 package com.example.assignmentvi_intent;
 
-public class SearchActivity {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.assignmentvi_intent.model.Meeting;
+import com.example.assignmentvi_intent.model.MeetingManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SearchActivity extends AppCompatActivity {
+    EditText searchTitle, searchPlace, searchParticipant, searchDate, searchTime;
+    Button btnSearch;
+    TextView resultText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        searchTitle = findViewById(R.id.search_title);
+        searchPlace = findViewById(R.id.search_place);
+        searchParticipant = findViewById(R.id.search_participants);
+        searchDate = findViewById(R.id.search_date);
+        searchTime = findViewById(R.id.search_time);
+        btnSearch = findViewById(R.id.btnSearch);
+        resultText = findViewById(R.id.search_result);
+
+        btnSearch.setOnClickListener(v -> {
+            String title = searchTitle.getText().toString();
+            String place = searchPlace.getText().toString();
+
+            String participantsText = searchParticipant.getText().toString();
+            List<String> participantsArray = Arrays.asList(participantsText.split(","));
+            List<String> participants = new ArrayList<>(participantsArray);
+
+            String date = searchDate.getText().toString();
+            String time = searchTime.getText().toString();
+
+            if (title.isEmpty() && place.isEmpty() && participants.isEmpty() && date.isEmpty() && time.isEmpty()){
+                resultText.setText("Please enter at least one search criteria.");
+                return;
+            }
+
+            List<Meeting> meetings = MeetingManager.searchMeeting(title, place, date, time, participants);
+
+            if (!meetings.isEmpty()){
+                StringBuilder resultBuilder = new StringBuilder();
+                for (Meeting meeting : meetings){
+                    resultBuilder.append("Title: ").append(meeting.getTitle()).append("\n")
+                            .append("Place: ").append(meeting.getPlace()).append("\n")
+                            .append("Participants: ").append(meeting.getParticipants()).append("\n")
+                            .append("Date: ").append(meeting.getDate()).append("\n")
+                            .append("Time: ").append(meeting.getTime()).append("\n\n");
+                }
+                resultText.setText(resultBuilder);
+            } else {
+                resultText.setText("No meetings found.");
+            };
+
+        });
+    }
+
 }
